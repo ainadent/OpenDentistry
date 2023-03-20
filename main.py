29a9674d1f26b573d6716a09dfbb8a5bc852d017
tensorboy <anwangpeng@gmail.com>
2023-03-20 13:59:21 -0700

    🎉 feat(main.py): add VTK file conversion functionality from STL to VTK format

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-dsfds
+import vtk
+
+input_file = "/Users/wangpengan/Desktop/startup/dental/oral_scan/nazhao/Nazhaooralscan/nazhao-stl/nazhao-stl UpperJawScan.stl"
+output_file = input_file.replace(".stl", ".vtk")
+
+# create reader for the STL file
+reader = vtk.vtkSTLReader()
+reader.SetFileName(input_file)
+reader.Update()
+
+# create writer for the VTK file
+writer = vtk.vtkDataSetWriter()
+writer.SetFileName(output_file)
+writer.SetInputConnection(reader.GetOutputPort())
+writer.Write()
+
